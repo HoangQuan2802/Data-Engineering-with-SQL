9862186c7bfe790eda8d8b880f0502619efb4d03
HoangQuan2802 <122335399+HoangQuan2802@users.noreply.github.com>
2023-09-04 20:29:23 +0700

    Add files via upload

---
+++ dependencies log table.sql
@@ -0,0 +1,24 @@
+﻿--	================Velocity==============
+--	Description: - This table to keep the track dependencies between object in the executed pipeline, it is done via Json @dependencies parameter
+--	======================================
+CREATE TABLE [etl].[dependencies_log](
+	[audit_id] [int] NOT NULL,								--	audit timestamp  in format yyyyMMddHHmmssfffffff
+	[parent_audit_id] [int] NOT NULL,						--	Optional parameter, 1 upper level audit timestamp (parent audit timestamp) in format yyyyMMddHHmmssfffffff, if equal to -1, it should equal to audit_ts
+	[root_id] [int] NOT NULL,								--	Optional parameter, the timestamp of the root caller in the calling hierarchy in format yyyyMMddHHmmssfffffff, if equal to -1, it should be audit_ts
+	[sysschema_name] [nvarchar](255) NOT NULL,				--  can be database name, file name
+	[host] [nvarchar](255) NOT NULL,						--  the location where the db or file is located
+	[object_type] [nvarchar](255) NULL,						--	type of object used in the executed proc, pipeline: table, view, file...
+	[object_name] [nvarchar](255) NOT NULL,					--	name of object used in the executed proc, pipeline.. including the object schema: sit.customer, stt.customer...
+	[object_role] [nvarchar](255) NOT NULL,					--	role of object used in the executed proc, pipeline.. does it used as lookup table, source or target
+PRIMARY KEY CLUSTERED 
+(
+	[audit_id] ASC,
+	[parent_audit_id] ASC,
+	[root_id] ASC,
+	[sysschema_name] ASC,
+	[host] ASC,
+	[object_name] ASC,
+	[object_role] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ save dependencies.sql
@@ -0,0 +1,78 @@
+﻿--	================Velocity==============
+--	Description: - This stored procedure is called to save the dependencies between within the executed pipeline 
+--				   via an input json-based parameter with below json structure
+--	======================================
+CREATE PROCEDURE sp_save_dependencies
+( 
+	 @parent_audit_id INT = -1 
+	,@root_id INT = -1 
+	,@audit_id INT
+	/*
+	This parameter is the json-based body of dependencies within the executed object
+
+	Json body:
+	[
+		{
+			"sysschema_name":"db1",								--can be database name, file name
+			"host_name":"spbt.database.windows.net",			--the location where the db or file is located
+			"object_type":"table",								-- type of object used in the executed proc, pipeline: table, view, file...
+			"object_name":"stt.test",							-- name of object used in the executed proc, pipeline.. including the object schema: sit.customer, stt.customer...
+			"object_role":"source"								-- role of object used in the executed proc, pipeline.. does it used as lookup table, source or target
+		},
+		{
+			"sysschema_name":"db2",								--can be database name, file name
+			"host_name":"spbt.database.windows.net",			--the location where the db or file is located
+			"object_type":"table",
+			"object_name":"stt.test",
+			"object_role":"lookup"
+		},
+		{
+			"sysschema_name":"db3",								--can be database name, file name
+			"host_name":"spbt.database.windows.net",			--the location where the db or file is located
+			"object_type":"table",
+			"object_name":"dmt.test",
+			"object_role":"target"
+		}
+	]
+	*/
+	,@dependencies NVARCHAR(MAX)
+)
+
+AS
+BEGIN TRY
+
+	INSERT INTO etl.dependencies_log
+    (
+		[audit_id]
+		,[parent_audit_id]
+		,[root_id]
+		,[sysschema_name]
+		,[host]
+		,[object_type]
+		,[object_name]
+		,[object_role]
+	)
+	SELECT @audit_id
+		   ,@parent_audit_id
+		   ,@root_id
+		   ,details.sysschema_name
+		   ,details.host_name
+		   ,details.object_type
+		   ,details.object_name
+		   ,details.object_role
+	FROM OPENJSON(@dependencies)
+		   WITH (
+				 sysschema_name nvarchar(255)
+				,[host_name] nvarchar(255)
+				,[object_type] nvarchar(255)
+				,[object_name] nvarchar(255)
+				,[object_role] nvarchar(255)
+			) details;
+
+END TRY
+
+BEGIN CATCH
+
+	THROW;
+
+END CATCH
